c7232aa3fd5244bdd3b045f92e05a5fd821cf895
Maurifisch <info@fischbein.de>
2023-04-15 17:10:19 +0200

    Initial Commit

---
+++ Lektion5/Lektion5App.swift
@@ -0,0 +1,17 @@
+//
+//  Lektion5App.swift
+//  Lektion5
+//
+//  Created by Mauri Fischbein on 15.04.23.
+//
+
+import SwiftUI
+
+@main
+struct Lektion5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
