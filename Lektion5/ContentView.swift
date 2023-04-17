dd6353ae00de78f29713fdab8b7a7e26c0fb0e70
Maurifisch <info@fischbein.de>
2023-04-17 09:53:24 +0200

    Version04172023

--- Lektion5/ContentView.swift
+++ Lektion5/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private  var messageString = "When the Genius Bar needs help, they call you!"
+    
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(messageString)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.red)
+                .padding()
+                .frame(width: 300, height: 150)
+                .border(.orange, width: 1)
+                
+
+           
+          
+               
+           HStack {
+               Button("Awesome") {
+                       messageString = "You Are Awesome!"
+                   }
+                   
+               .buttonStyle(.borderedProminent)
+               
+               Button("Great"){
+                   messageString = " You Are Great"
+           }
+               
+               
+               }
+               .buttonStyle(.borderedProminent)
+               
+           
+           // Image(systemName: "swift")
+              //  .resizable()
+                //.scaledToFit()
+              //  .foregroundColor(.orange)
+              //  .padding()
+               .border(.purple, width: 5)
         }
-        .padding()
+       
     }
 }
 
